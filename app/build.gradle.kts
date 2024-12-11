3dfd80e2447b7f5e8edb5023818989e7b6e0f068
hemou6 <184505870+hemou6@users.noreply.github.com>
2024-12-11 14:15:44 +0800

    实现搜索全球城市数据功能

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,13 +38,21 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation("com.google.code.gson:gson:2.8.8")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.squareup.retrofit2:retrofit:2.6.1")
+    implementation("com.squareup.retrofit2:converter-gson:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.0.0")
+    implementation("com.google.android.material:material:1.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.1.1")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.sunnyweather.ui.place
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.map
+import androidx.lifecycle.switchMap
+import com.example.sunnyweather.logic.Repository
+import com.example.sunnyweather.logic.model.Place
+import retrofit2.http.Query
+
+class PlaceViewModel:ViewModel() {
+    private val searchLiveData=MutableLiveData<String>()
+
+    val placeList=ArrayList<Place>()
+
+    val placeLiveData=searchLiveData.switchMap { query->
+        Repository.searchPlaces(query)
+    }
+    
+    fun searchPlaces(query: String){
+        searchLiveData.value=query
+    }
+}
